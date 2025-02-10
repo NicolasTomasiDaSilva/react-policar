e41b77138450c89971abc73b7f3855da0f809cb2
Nicolas Tomasi da Silva <tomasitrabalho@gmail.com>
2025-02-10 17:27:05 -0300

    criar menu hamburger e melhorias feitas

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,40 @@
 import styles from "./Header.module.css";
+import Hamburger from "hamburger-react";
+
+import React, { useState, useEffect } from "react";
 
 interface HeaderProps {
   scrolled: boolean;
 }
 
 function Header({ scrolled }: HeaderProps) {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <header className={styles.header + " " + (scrolled ? styles.scrolled : "")}>
-      <h1>Mecanica Policar</h1>
-      <nav>
+      <h1>Policar</h1>
+      <div className={styles.hamburger}>
+        <Hamburger size={25} color="white" toggled={open} toggle={setOpen} />
+        {
+          <nav
+            className={`${styles.navHamburger} ${open ? styles.active : ""}`}
+          >
+            <ul>
+              <li>
+                <a href="#">Inicio</a>
+              </li>
+              <li>
+                <a href="#">Sobre</a>
+              </li>
+              <li>
+                <a href="#">Contato</a>
+              </li>
+            </ul>
+          </nav>
+        }
+      </div>
+
+      <nav className={styles.nav}>
         <ul>
           <li>
             <a href="#">Inicio</a>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,17 @@
 import styles from "./Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 function Home() {
-  return <div className={styles.home}>Home</div>;
+  return (
+    <div className={styles.home}>
+      <h2>Precisa de ajuda com o seu veículo?</h2>
+      <a>
+        <FontAwesomeIcon icon={faWhatsapp} />
+        FALE CONOSCO
+      </a>
+    </div>
+  );
 }
 
 export default Home;
